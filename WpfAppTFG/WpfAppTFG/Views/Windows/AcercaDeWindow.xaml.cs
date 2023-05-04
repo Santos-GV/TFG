70f21620f7ab7b8e368e809896477fbc4757eec9
Santos-GV <santosgvdam@gmail.com>
2023-05-04 18:21:59 +0200

    Add AcercaDe window and some events
    Actualizar AcercaDeWindow.xaml, AcercaDeWindow.xaml.cs y 2 más archivos...
    Fix window centering when open
    Add salir, cerrar sesión, administrarusuarios, acerca de eventos

---
+++ WpfAppTFG/WpfAppTFG/Views/Windows/AcercaDeWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace WpfAppTFG.Views.Windows
+{
+    /// <summary>
+    /// Lógica de interacción para AcercaDe.xaml
+    /// </summary>
+    public partial class AcercaDeWindow : Window
+    {
+        public AcercaDeWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- WpfAppTFG/WpfAppTFG/Views/Windows/HomeWindow.xaml.cs
+++ WpfAppTFG/WpfAppTFG/Views/Windows/HomeWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 using WpfAppTFG.Views.Pages;
 
 namespace WpfAppTFG.Views.Windows
@@ -17,6 +19,36 @@ namespace WpfAppTFG.Views.Windows
         {
             var usersAdminPage = new UsersAdminPage();
             pagesContainer.Navigate(usersAdminPage);
+        }
+
+        private void MenuBar_acercaDeEvento()
+        {
+            var acerdaDeWindow = new AcercaDeWindow();
+            acerdaDeWindow.Owner = this;
+            acerdaDeWindow.ShowDialog();
+        }
+
+        private void MenuBar_salirEvento()
+        {
+            // Cierra la aplicación entera
+            // tanto la ventana padre, como esta
+            Application.Current.Shutdown();
+        }
+
+        private void MenuBar_cerrarSesionEvento()
+        {
+            try
+            {
+                Owner.Visibility = Visibility.Visible;
+                this.Close();
+            }
+            catch (InvalidOperationException) { }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // Al cerrar la ventana se cierra sesión
+            MenuBar_cerrarSesionEvento();
         }
     }
 }
