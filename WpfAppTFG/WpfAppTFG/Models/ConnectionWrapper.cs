cb9d9e0d57edeb15a4e294f5df9c4f0dbfd1d02a
Santos-GV <santosgvdam@gmail.com>
2023-05-30 18:33:07 +0200

    Add connection string to ConnectionWrapper
    Actualizar ConnectionWrapper.cs

--- WpfAppTFG/WpfAppTFG/Models/ConnectionWrapper.cs
+++ WpfAppTFG/WpfAppTFG/Models/ConnectionWrapper.cs
@@ -1,6 +1,10 @@
 ﻿namespace WpfAppTFG.Model
 {
     using MongoDB.Driver;
+    using System;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Resources;
 
     /// <summary>
     /// Encapsulador de la conexión a la base de datos.
@@ -23,7 +27,11 @@
                 // Si la conexión no existe (es null) se crea la conexión
                 if (instance.Client == null)
                 {
-                    instance.Client = new MongoClient("mongodb://localhost:27017");
+                    var resourceDictionary = new ResourceDictionary();
+                    resourceDictionary.Source = new Uri("pack://application:,,,/WpfAppTFG;component/ConnectionStringDictionary.xaml", UriKind.Absolute);
+                    // Access a specific resource by key
+                    var connectionString = resourceDictionary["connectionString"].ToString();
+                    instance.Client = new MongoClient(connectionString);
                     instance.Database = instance.Client.GetDatabase("ForoSaber");
                 }
                 return instance;
