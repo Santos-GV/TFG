e3f95398a55bc92d21da228bdaf65dee92ffe39d
Santos-GV <santosgvdam@gmail.com>
2023-06-01 10:07:15 +0200

    Add Login logs
    Update LoginController.cs and LoginPage.xaml.cs
    
    Fix LoginPage & Controller code
    Add Log logic

--- WpfAppTFG/WpfAppTFG/Controllers/LoginController.cs
+++ WpfAppTFG/WpfAppTFG/Controllers/LoginController.cs
@@ -1,17 +1,73 @@
 ﻿using MongoDB.Driver.Linq;
 using System.Threading.Tasks;
+using System.Windows;
 using WpfAppTFG.Model;
 using WpfAppTFG.Model.Respository;
+using WpfAppTFG.Views.Pages;
+using WpfAppTFG.Views.Shareds;
+using WpfAppTFG.Views.Windows;
 
 namespace WpfAppTFG.Controller
 {
     public class LoginController
     {
         private readonly UserRepository userRepository;
+        private readonly LogRepository logRepository;
+        public User? logginUser;
+        private readonly LoginPage view;
 
-        public LoginController()
+        public LoginController(LoginPage view)
         {
             userRepository = new UserRepository();
+            logRepository = new LogRepository();
+            this.view = view;
+        }
+
+        /// <summary>
+        /// Intenta logear el usuario
+        /// </summary>
+        /// <returns></returns>
+        public async Task TryLogin()
+        {
+            var userTask = Login(view.userName.Text, view.psswd.Password);
+            if (string.IsNullOrEmpty(view.userName.Text) || string.IsNullOrEmpty(view.psswd.Password))
+            {
+                view.info.Text = "Rellena todos los campos";
+                return;
+            }
+            var user = await userTask;
+            if (user is not null)
+            {
+                logginUser = user;
+                view.info.Text = string.Empty;
+                view.userName.Text = string.Empty;
+                view.psswd.Password = string.Empty;
+                await Login();
+            }
+            else
+            {
+                view.info.Text = "El usuario y/o contraseña no son válidos";
+                view.psswd.Password = string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Abre la ventana de inicio
+        /// </summary>
+        private async Task Login()
+        {
+            WindowProperties.SetWindowVisibility(Visibility.Hidden, view);
+            var user = logginUser;
+            if (user is null)
+            {
+                MessageBox.Show("No se pudo abrir la aplicación", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            var log = new Log(user.Id, $"Hace login");
+            await logRepository.Create(log);
+            var homeWindow = new HomeWindow(user);
+            WindowProperties.SetWindowOwner(homeWindow, view);
+            homeWindow.Show();
         }
 
         /// <summary>
@@ -31,6 +87,23 @@ namespace WpfAppTFG.Controller
             var isPsswdCorrect = user.CheckPsswd(userPsswd);
             if (!isPsswdCorrect) return null;
             return user;
+        }
+
+        /// <summary>
+        /// Cambia el título de la ventana
+        /// </summary>
+        public void ChangeWindowTitle()
+        {
+            WindowProperties.SetWindowTitle("Login", view);
+        }
+
+        /// <summary>
+        /// Navega a la página de regitro
+        /// </summary>
+        public void Registrarse()
+        {
+            var registerPage = new RegisterPage();
+            view.NavigationService.Navigate(registerPage);
         }
     }
 }
--- WpfAppTFG/WpfAppTFG/Views/Pages/LoginPage.xaml.cs
+++ WpfAppTFG/WpfAppTFG/Views/Pages/LoginPage.xaml.cs
@@ -1,11 +1,6 @@
-﻿using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using WpfAppTFG.Controller;
-using WpfAppTFG.Model;
-using WpfAppTFG.Model.Respository;
-using WpfAppTFG.Views.Shareds;
-using WpfAppTFG.Views.Windows;
 
 namespace WpfAppTFG.Views.Pages
 {
@@ -15,65 +10,25 @@ namespace WpfAppTFG.Views.Pages
     public partial class LoginPage : Page
     {
         private readonly LoginController controller;
-        public User? logginUser;
 
         public LoginPage()
         {
             InitializeComponent();
-            controller = new LoginController();
+            controller = new LoginController(this);
         }
         private void login_Loaded(object sender, RoutedEventArgs e)
         {
-            WindowProperties.SetWindowTitle("Login", this);
-        }
-
-        private void RegistrarseEvento()
-        {
-            var registerPage = new RegisterPage();
-            this.NavigationService.Navigate(registerPage);
-        }
-
-        private void LoginEvento()
-        {
-            WindowProperties.SetWindowVisibility(Visibility.Hidden, this);
-            var user = logginUser;
-            if (user is null)
-            {
-                MessageBox.Show("No se pudo abrir la aplicación", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-                return;
-            }
-            var homeWindow = new HomeWindow(user);
-            WindowProperties.SetWindowOwner(homeWindow, this);
-            homeWindow.Show();
+            controller.ChangeWindowTitle();
         }
 
         private async void login_Click(object sender, RoutedEventArgs e)
         {
-            var userTask = controller.Login(userName.Text, psswd.Password);
-            if (string.IsNullOrEmpty(userName.Text) || string.IsNullOrEmpty(psswd.Password))
-            {
-                info.Text = "Rellena todos los campos";
-                return;
-            }
-            var user = await userTask;
-            if (user is not null)
-            {
-                logginUser = user;
-                info.Text = string.Empty;
-                userName.Text = string.Empty;
-                psswd.Password = string.Empty;
-                LoginEvento();
-            }
-            else
-            {
-                info.Text = "El usuario y/o contraseÃ±a no son vÃ¡lidos";
-                psswd.Password = string.Empty;
-            }
+            await controller.TryLogin();
         }
 
         private void register_Click(object sender, RoutedEventArgs e)
         {
-            RegistrarseEvento();
+            controller.Registrarse();
         }
     }
 }
